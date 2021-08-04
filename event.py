01b985090de7fb3dc04df18b6dd727c5d68c4afd
Artemtar05 <88440634+Artemtar05@users.noreply.github.com>
2021-08-04 16:28:23 +0300

    Управление персонажем

---
+++ event.py
@@ -0,0 +1,36 @@
+import pygame
+
+pygame.init()
+
+SIZE = [600, 400]
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption('Заголовок окна')
+pygame.display.set_icon(pygame.image.load('icon.png.png'))
+
+clock = pygame.time.Clock()
+FPS = 60
+
+WHITE = (255, 255, 255)
+RED = (128, 0, 0)
+YELLOW = (218, 165, 32)
+x = 300
+y = 200
+speed = 25
+
+flag = True
+while flag:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                x += speed
+            elif event.key == pygame.K_a:
+                x -= speed
+            elif event.key == pygame.K_w:
+                y -= speed
+            elif event.key == pygame.K_s:
+                y += speed
+    pygame.draw.rect(screen, YELLOW, (x, y, 10, 20))
+    pygame.display.update()
+    clock.tick(FPS)
